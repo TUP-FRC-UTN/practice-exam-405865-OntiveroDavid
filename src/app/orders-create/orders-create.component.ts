75555419f99f163338024ccc1d36a533bc3d5764
405865-OntiveroDavid <405865@tecnicatura.frc.utn.edu.ar>
2024-11-01 18:47:38 -0300

    implementando formGroup

--- src/app/orders-create/orders-create.component.ts
+++ src/app/orders-create/orders-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-orders-create',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './orders-create.component.css'
 })
 export class OrdersCreateComponent {
+  myForm: FormGroup;
 
+  constructor() {
+    this.myForm = new FormGroup({   
+      nombre: new FormControl('', [Validators.required ,Validators.min(3)]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      // Implementar enviar los datos
+      console.log(this.myForm.value);
+    }
+  }
 }
